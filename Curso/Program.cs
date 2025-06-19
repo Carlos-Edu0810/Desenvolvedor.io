2e19285d783fb8989b6a949a6ae29bab29a73610
Carlos-Edu0810 <ramos0810eduardo@gmail.com>
2025-06-18 23:57:09 -0300

    criando verificação de novas migrations

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.EntityFrameworkCore;
+
+using var db = new CursoEFCore.Data.ApplicationContext();
+//db.Database.Migrate();
+
+var MigrationPendente = db.Database.GetPendingMigrations().Any();
+if (MigrationPendente)
+{
+    // regra de negocio...
+}
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
